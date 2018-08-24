481580330559ffe32c2e9401a4ebb7ffa0771e03
Stephen Franceschelli <Stephen.Franceschelli@microsoft.com>
2018-08-24 12:59:57 -0400

    Use correct folder for test workspace.

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,36 +1,70 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
 
 // This suite performs validation tests that look at validating yaml files.
 // The tests are looking at if there are any file validation errors, and if there are, what are they.
 // 
 // These tests need to ensure validation errors are propagated to the ui, we do not need to test
 // every type and permutation of validation errors, that should be handled in unit tests.
+const extensionId = 'ms-azure-devops.azure-pipelines';
+
 suite ('Extension Setup Tests', function() {
     this.timeout(20000);
 
     // TODO: Run this before every test?
     test("Extension is active", async () => {
-        await sleep(2000);
-
         // Arrange and Act
-        const started = vscode.extensions.getExtension("ms-azure-devops.azure-pipelines").isActive;
-        console.log('pipelines extension started: ' + started);
-
-        // TODO: It's not active because we haven't chosen a file to activate it?
+        await sleep(2000);
+        const started = vscode.extensions.getExtension(extensionId).isActive;
 
         // Assert
         assert.equal(started, true);
     });
 });
 
-async function sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 suite('Validation Tests', function() {
-    test ('Given an empty document, there should be no validation errors', function() {
+    test ('Given an empty document, there should be no validation errors', async () => {
+        // load an empty document
+        // mark it as an azure pipelines file
+        // check for validation errors, shouldn't be any
+
+
+
+
+        //const extension = await vscode.extensions.getExtension(extensionId);
+
+
+
+
+
+         // TODO: What is the workspace that is loaded when the extension runs? Should we create one?
+
+        const emptyFile: vscode.Uri = await vscode.workspace.findFiles('emptyfile.yml')[0];
+        const emptyDocument: vscode.TextDocument = await vscode.workspace.openTextDocument(emptyFile);
+        //const visibleDocument: vscode.TextEditor = await vscode.window.showTextDocument(emptyDocument); // KEEP THIS
+
+        // TODO: This should be azure pipelines.
+        console.log(emptyDocument.languageId);
+
+        // Check the type of the current text document
+        const diagnostics: vscode.Diagnostic[] = vscode.languages.getDiagnostics(emptyFile);
+        console.log(JSON.stringify(diagnostics));
+
+        // Check the type of the current text document
+            
+
+
+        // Check validation errors for current document, check the name of what is set by the server?
+        //visibleDocument.selection
+
+
+        // The file should already be identified as an azure pipelines file, can we validate that?
+
+
+        // Now make sure there are no validation errors
+
+
+
         // Arrange
 
 
@@ -62,6 +96,35 @@ suite('Validation Tests', function() {
         // Assert
         
     });
+
+    test ('Manually selecting file type as Azure Pipelines works', function() {
+       
+        //languageId
+        // before manual activation, language id is text or yaml, after manual activation the id should be azure pipelines
+
+    });
+
+    test ('When manually activating an invalid file there should be validation errors', function() {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+        
+    });
+
+    test ('When manually activating a valid file there should not be validation errors', function() {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+        
+    });
 });
 
 // This suite performs autocomplete tests that look for what options are available for autocompletion
@@ -74,3 +137,7 @@ suite('Autocomplete Tests', function() {
         
     });
 })
+
+async function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
