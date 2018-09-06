bf400e20985c0f1ada9cbf2105ce8c22dd158680
Stephen Franceschelli <Stephen.Franceschelli@microsoft.com>
2018-09-06 14:35:28 -0400

    Initial.

---
+++ src/testfromserver/fromserver.test.ts
@@ -0,0 +1,55 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+// This suite performs validation tests that look at validating yaml files.
+// The tests are looking at if there are any file validation errors, and if there are, what are they.
+// 
+// These tests need to ensure validation errors are propagated to the ui, we do not need to test
+// every type and permutation of validation errors, that should be handled in unit tests.
+const extensionId = 'ms-azure-devops.azure-pipelines';
+
+suite ('Extension Setup Tests', function() {
+    this.timeout(20000);
+
+    test("Extension is active", async () => {
+        // Arrange and Act
+        await sleep(2000);
+        const started = vscode.extensions.getExtension(extensionId).isActive;
+
+        // Assert
+        assert.equal(started, true);
+    });
+});
+
+// Helpers
+// 1. Workspace configuration settings are not as expected
+//    console.log('workspace configuration: ' + JSON.stringify(vscode.workspace.getConfiguration()));
+// 2. 
+
+suite('Validation Tests From Server', function() {
+    this.timeout(200000);
+
+    test ('Given a valid document, there should be no validation errors', async () => {
+        // Arrange
+        const emptyFiles: vscode.Uri[] = await vscode.workspace.findFiles('validfile.yml');
+        const emptyFile: vscode.Uri = emptyFiles[0];
+
+        // Act
+        const emptyDocument: vscode.TextDocument = await vscode.workspace.openTextDocument(emptyFile);
+        await vscode.window.showTextDocument(emptyDocument);
+        await sleep(3100); // Give it time to show the validation errors, if any
+        const diagnostics: vscode.Diagnostic[] = vscode.languages.getDiagnostics(emptyFile);
+
+        // Assert
+        assert.equal(emptyDocument.languageId, 'azure-pipelines');
+        assert.equal(diagnostics.length, 0);
+    });
+});
+
+async function verifyDocumentIsValid() {
+
+}
+
+async function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
