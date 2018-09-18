d93c3e8901ede44fbd3693a1b96142da131b2f92
Matt Cooper <mattc@xbox.com>
2018-09-18 07:58:51 -0400

    id change

--- src/schemata.ts
+++ src/schemata.ts
@@ -321,7 +321,7 @@ const phasesAtRoot140 = augment({
 
 export const schema140: string = JSON.stringify({
     "$schema": "http://json-schema.org/draft-07/schema#",
-    "$id": "https://github.com/Microsoft/vsts-agent/blob/master/src/Misc/ci-schema.json",
+    "$id": "https://github.com/Microsoft/azure-pipelines-vscode/blob/master/local-schema.json",
     "$comment": "v1.140.2",
     "title": "Pipeline schema",
     "description": "A pipeline definition",
