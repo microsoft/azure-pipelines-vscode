4663ee4ef7ad6c59e07ae79a488284c0fa29cfc9
Disha Khakhar <dikhakha@microsoft.com>
2019-08-29 11:55:52 +0530

    Remove unnecessary links in messages (#49)
    * Remove unnecessary links in messages
    
    * review comments

--- src/configure/resources/messages.ts
+++ src/configure/resources/messages.ts
@@ -5,10 +5,10 @@ export class Messages {
     public static appKindIsNotSupported: string = 'App of kind: %s is not yet supported.';
     public static azureAccountExntesionUnavailable: string = 'Azure-Account extension could not be fetched. Kindly check it is installed and activated.';
     public static azureLoginRequired: string = 'Kindly sign in to your Azure Account before going forward.';
-    public static branchRemoteMissing: string = `The current branch does not have a remote tracking branch and the selected repository has no remotes. Kindly, [set a remote tracking branch](https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---track) with [Azure Repos](https://docs.microsoft.com/en-us/azure/devops/repos/get-started) or [Github](https://guides.github.com/activities/hello-world/) to procceed.`;
+    public static branchRemoteMissing: string = `The current branch does not have a remote tracking branch and the selected repository has no remotes. Kindly, [set a remote tracking branch](https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---track) with Azure Repos or GitHub to procceed.`;
     public static browsePipeline: string = 'Browse Pipeline';
     public static cannotAddFileRemoteMissing: string = 'Cannot add yml file to your git repository, remote is not set';
-    public static cannotIdentifyRespositoryDetails: string = 'Could not identify repository details. Ensure your git repo is managed with [Azure Repos](https://docs.microsoft.com/en-us/azure/devops/repos/get-started) or [Github](https://guides.github.com/activities/hello-world/)';
+    public static cannotIdentifyRespositoryDetails: string = 'Could not identify repository details. Ensure your git repo is managed with Azure Repos or GitHub';
     public static commitAndPush: string = 'Commit & Push';
     public static commitFailedErrorMessage: string =`Commit failed due to error: %s`;
     public static configuringPipelineAndDeployment: string = 'Configuring Azure DevOps Pipeline and proceeding to deployment...';
@@ -26,7 +26,7 @@ export class Messages {
     public static githubPatTokenHelpMessage: string = 'GitHub PAT token with following permissions: full access of repository webhooks and services, read and write access to all repositories data.';
     public static modifyAndCommitFile: string = 'Modify and save your yaml file. %s will commit this file, push the branch \'%s\' to remote \'%s\' and proceed with deployment.';
     public static noAgentQueueFound: string = 'No Agent Queue found with pool name: %s';
-    public static notAGitRepository: string = 'Selected workspace is not a [git](https://git-scm.com/docs/git) repository. Please select a git repository.';
+    public static notAGitRepository: string = 'Selected workspace is not a git repository. Please select a git repository.';
     public static notAzureRepoUrl: string = 'Repo Url is not of Azure Repos type.';
     public static noWorkSpaceSelectedError: string = 'You need to select a workspace folder to configure pipeline.';
     public static operationCancelled: string = 'Operation cancelled.';
@@ -34,12 +34,11 @@ export class Messages {
     public static organizationNameReservedMessage: string = 'The organization name %s is not available. Please try another organization name';
     public static organizationNameStaticValidationMessage: string = 'Organization names must start and end with a letter or number and can contain only letters, numbers, and hyphens';
     public static pipelineSetupSuccessfully: string = 'Azure DevOps pipeline set up successfully !';
-    public static remoteRepositoryNotConfigured: string = 'Remote repository is not configured. Manage your git repository with [Azure Repos](https://docs.microsoft.com/en-us/azure/devops/repos/get-started) or [Github](https://guides.github.com/activities/hello-world/)';
+    public static remoteRepositoryNotConfigured: string = 'Remote repository is not configured. Manage your git repository with Azure Repos or GitHub';
     public static resourceIdMissing: string = 'Required argument: resourceId, is missing. Kindly pass the argument for getting resource.';
     public static resourceTypeIsNotSupported: string = 'Resource of type: %s is not yet supported for configuring pipelines.';
     public static selectFolderLabel: string = 'Select source folder for configuring pipeline';
     public static selectOrganization: string = 'Select Azure DevOps Organization';
-    public static selectPathToAppSourceCode: string = 'Select the path to your application source code.';
     public static selectPipelineTemplate: string = 'Select Azure pipelines template...';
     public static selectProject: string = 'Select Azure DevOps project';
     public static selectRemoteForBranch: string = 'Select the remote repository where you want to track your current branch';
