c5baacd368785c05b03d6ad1abd172e98b5d8d26
Pier-Luc Michaud <pier-Luc.michaud@hiloenergie.com>
2020-08-06 17:21:47 -0400

    Allow paths relative to workspace for custom schema

--- src/schema-association-service.ts
+++ src/schema-association-service.ts
@@ -23,7 +23,9 @@ export class SchemaAssociationService implements ISchemaAssociationService {
     }
 
     public locateSchemaFile() {
-        const alternateSchema = vscode.workspace.getConfiguration('[azure-pipelines]', null).get<string>('customSchemaFile');
+        let alternateSchema = vscode.workspace.getConfiguration('[azure-pipelines]', null).get<string>('customSchemaFile');
+        if (alternateSchema && alternateSchema.startsWith('./'))
+            alternateSchema = path.resolve(vscode.workspace.rootPath, alternateSchema);
         const schemaPath = alternateSchema || path.join(this.extensionPath, './service-schema.json');
         this.schemaFilePath = vscode.Uri.file(schemaPath).toString();
     }
