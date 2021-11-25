3decd59fb56a20eec2a0628d2dbbda7fb9cfc43c
Winston Liu <Wliu1402@gmail.com>
2021-11-24 20:22:53 -0500

    Install azure-account

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,6 +1,11 @@
+import * as cp from 'child_process';
 import * as path from 'path';
 
-import { runTests } from '@vscode/test-electron';
+import {
+  downloadAndUnzipVSCode,
+  resolveCliPathFromVSCodeExecutablePath,
+  runTests
+} from '@vscode/test-electron';
 
 async function main() {
   try {
@@ -17,8 +22,21 @@ async function main() {
     // workspace isn't copied to out because it's all YAML files.
     const launchArgs = [path.resolve(__dirname, '../../src/test/workspace')];
 
+    const vscodeExecutablePath = await downloadAndUnzipVSCode();
+    const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+
+    cp.spawnSync(cliPath, ['--install-extension', 'ms-vscode.azure-account'], {
+      encoding: 'utf-8',
+      stdio: 'inherit'
+    });
+
     // Download VS Code, unzip it and run the integration test
-    await runTests({extensionDevelopmentPath, extensionTestsPath, launchArgs});
+    await runTests({
+      vscodeExecutablePath,
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs
+    });
   } catch (err) {
     console.error(err);
     console.error('Failed to run tests');
